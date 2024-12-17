438434bfd44b747049c080e860addbd9eaf40efa
felixp038 <phanfelix1@gmail.com>
2024-12-17 22:45:45 +0000

    commit2

--- classwork.cpp
+++ classwork.cpp
@@ -6,6 +6,38 @@
 
 int main()
 {
-
+  int user_input = 0;
+  int num1;
+  int num2;
+  int numerator;
+  int denominator;
   return 0;
+  while (user_input != 99)
+  {
+    std::cout << "Welcome to C++ Calculator!  Choose an option below!\n"    
+              << "1. Add\n"
+              << "2. Subtract\n"
+              << "3. Multiply\n"
+              << "4. Divide\n"
+              << "5. Square root\n"
+              << "99. Quit\n\n> ";
+  }
+
+    std::cin >> user_input;
+  if (user_input == 1)
+    {
+    std::cout << "Enter two numbers\n";
+    std::cin >> num1 >> num2;
+    std::cout << "Their sum is " << (num1 + num2) << std::endl;
+    }
+  else{
+    if (user_input == 2)
+    {
+      std::cout << "Enter two numbers\n";
+      std::cin >> num1 >> num2;
+      std::cout << "Their sum is " << (num1 - num2) << std::endl;
+    }
+
+  }
+    
 }
